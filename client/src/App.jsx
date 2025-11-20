10127c21e7270e3ef12f22908c74c0d6aab8f851
Shithi Moni <shithim555@gmail.com>
2025-11-20 14:42:07 +0600

    general update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,153 +1,151 @@
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./components/auth/layout";
-import AuthLogin from "./pages/auth/login";
-import AuthRegister from "./pages/auth/register";
-import AdminLayout from "./components/admin-view/layout";
-import AdminDashboard from "./pages/admin-view/dashboard";
-import AdminProducts from "./pages/admin-view/products";
-import AdminOrders from "./pages/admin-view/oders";
-import AdminFeatures from "./pages/admin-view/features";
-import ShoppingLayout from "./components/shopping-view/layout";
-import NotFound from "./pages/not-found";
-import ShoppingHome from "./pages/shopping-view/home";
-import ShoppingListing from "./pages/shopping-view/listing";
-import ShoppingCheckout from "./pages/shopping-view/checkout";
-import ShoppingAccount from "./pages/shopping-view/account";
-import CheckAuth from "./components/common/check-auth";
-import Unauthorized from "./pages/unauthorized";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { checkAuth } from "./store/auth-slice";
-import { Skeleton } from "@/components/ui/skeleton";
-import StripeSuccess from "./pages/shopping-view/StripeSuccess";
-import SearchProducts from "./pages/shopping-view/search";
-import AuctionProductsView from "./pages/admin-view/auction-products";
-import AuctionPage from "./pages/shopping-view/AuctionPage";
-import AuctionDetails from "./pages/shopping-view/auctionDetails";
-import AuctionCheckout from "./pages/shopping-view/auction-checkout";
-import AuctionStripeSuccess from "./pages/shopping-view/AuctionStripeSuccess";
-import AdminAuctionOrders from "./pages/admin-view/AdminAuctionOrders";
-import AdminUsers from "./pages/admin-view/AdminUsers";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import AdminLayout from './components/admin-view/layout';
+import AuthLayout from './components/auth/layout';
+import CheckAuth from './components/common/check-auth';
+import Loading from './components/common/loading-component';
+import ShoppingLayout from './components/shopping-view/layout';
+import AdminAuctionOrders from './pages/admin-view/AdminAuctionOrders';
+import AdminProducts from './pages/admin-view/AdminProducts';
+import AdminUsers from './pages/admin-view/AdminUsers';
+import AllNotifications from './pages/admin-view/AllNotifications';
+import AuctionProductsView from './pages/admin-view/AuctionProductsView';
+import AuctionWinners from './pages/admin-view/AuctionWinners';
+import Feedbacks from './pages/admin-view/Feedbacks';
+import AdminDashboard from './pages/admin-view/dashboard';
+import AdminFeatures from './pages/admin-view/features';
+import AdminOrders from './pages/admin-view/oders';
+import ForgotPassword from './pages/auth/forgot-password';
+import AuthLogin from './pages/auth/login';
+import AuthRegister from './pages/auth/register';
+import ResetPassword from './pages/auth/reset-password';
+import VerifyEmail from './pages/auth/verify-email';
+import VerifyTokenPage from './pages/auth/verify-token';
+import NotFound from './pages/not-found';
+import AuctionPage from './pages/shopping-view/AuctionPage';
+import AuctionStripeSuccess from './pages/shopping-view/AuctionStripeSuccess';
+import StripeSuccess from './pages/shopping-view/StripeSuccess';
+import AboutUsPage from './pages/shopping-view/about-us';
+import ShoppingAccount from './pages/shopping-view/account';
+import AuctionCheckout from './pages/shopping-view/auction-checkout';
+import ShoppingCheckout from './pages/shopping-view/checkout';
+import Contact from './pages/shopping-view/contact';
+import ShoppingHome from './pages/shopping-view/home';
+import ShoppingListing from './pages/shopping-view/listing';
+import SearchProducts from './pages/shopping-view/search';
+import Unauthorized from './pages/unauthorized';
+import { checkAuth } from './store/auth-slice';
 
 function App() {
-	const { user, isAuthenticated, isLoading } = useSelector(
-		(state) => state.auth
-	);
+  const { user, isAuthenticated, isLoading } = useSelector((state) => state.auth);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(checkAuth());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
 
-	if (isLoading) {
-		return <Skeleton className="w-[600px] h-[600px] rounded-full" />;
-	}
+  if (isLoading) {
+    return <Loading />;
+  }
 
-	return (
-		<div className="flex flex-col overflow-hidden bg-white">
-			<Routes>
-				<Route
-					path="/auth"
-					element={
-						<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-							<AuthLayout />
-						</CheckAuth>
-					}
-				>
-					<Route path="login" element={<AuthLogin />} />
-					<Route path="register" element={<AuthRegister />} />
-				</Route>
-				<Route
-					path="/admin"
-					element={
-						<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-							<AdminLayout />
-						</CheckAuth>
-					}
-				>
-					<Route path="dashboard" element={<AdminDashboard />} />
-					<Route path="products" element={<AdminProducts />} />
-					<Route path="auction-products" element={<AuctionProductsView />} />
-					<Route path="orders" element={<AdminOrders />} />
-					<Route path="auction-orders" element={<AdminAuctionOrders />} />
-					<Route path="features" element={<AdminFeatures />} />
-					<Route path="users" element={<AdminUsers />} />
-				</Route>
+  return (
+    <div className="flex flex-col overflow-hidden bg-white font-dmSans">
+      <Routes>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
 
-				{/* <Route
-					path="/shop"
-					element={
-						<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-							<ShoppingLayout />
-						</CheckAuth>
-					}
-				>
-					<Route path="home" element={<ShoppingHome />} />
-					<Route path="listing" element={<ShoppingListing />} />
-					<Route path="checkout" element={<ShoppingCheckout />} />
-					<Route path="account" element={<ShoppingAccount />} />
-				</Route> */}
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="auction-products" element={<AuctionProductsView />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="auction-orders" element={<AdminAuctionOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+          <Route path="users" element={<AdminUsers />} />
+          <Route path="auction-winners" element={<AuctionWinners />} />
+          <Route path="feedbacks" element={<Feedbacks />} />
+          <Route path="notifications" element={<AllNotifications />} />
+        </Route>
 
-				<Route path="/" element={<ShoppingLayout />}>
-					{/* Public routes */}
-					<Route path="/" element={<ShoppingHome />} />
-					<Route path="shop/listing" element={<ShoppingListing />} />
-					<Route path="shop/auction" element={<AuctionPage />} />
-					<Route
-						path="shop/auction-product-details/:id"
-						element={<AuctionDetails />}
-					/>
-					<Route path="shop/search" element={<SearchProducts />} />
+        <Route path="/" element={<ShoppingLayout />}>
+          {/* Public routes */}
+          <Route path="/" element={<ShoppingHome />} />
+          <Route path="about-us" element={<AboutUsPage />} />
+          <Route path="shop/listing" element={<ShoppingListing />} />
+          <Route path="shop/auction" element={<AuctionPage />} />
+          <Route path="shop/search" element={<SearchProducts />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
+          <Route path="verify-email" element={<VerifyEmail />} />
+          <Route path="verify/:token" element={<VerifyTokenPage />} />
+          <Route path="contact" element={<Contact />} />
 
-					{/* Protected routes */}
-					<Route
-						path="shop/checkout"
-						element={
-							<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-								<ShoppingCheckout />
-							</CheckAuth>
-						}
-					/>
-					<Route
-						path="auction/checkout/:id"
-						element={
-							<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-								<AuctionCheckout />
-							</CheckAuth>
-						}
-					/>
-					<Route
-						path="auction/stripe-success"
-						element={
-							<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-								<AuctionStripeSuccess />
-							</CheckAuth>
-						}
-					/>
-					<Route
-						path="shop/account"
-						element={
-							<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-								<ShoppingAccount />
-							</CheckAuth>
-						}
-					/>
-					<Route
-						path="shop/stripe-success"
-						element={
-							<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-								<StripeSuccess />
-							</CheckAuth>
-						}
-					/>
-				</Route>
+          {/* Protected routes */}
+          <Route
+            path="shop/checkout"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <ShoppingCheckout />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="auction/checkout/:id"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <AuctionCheckout />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="auction/stripe-success"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <AuctionStripeSuccess />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="shop/account"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <ShoppingAccount />
+              </CheckAuth>
+            }
+          />
+          <Route
+            path="shop/stripe-success"
+            element={
+              <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                <StripeSuccess />
+              </CheckAuth>
+            }
+          />
+        </Route>
 
-				<Route path="*" element={<NotFound />} />
-				<Route path="/unauthorized" element={<Unauthorized />} />
-			</Routes>
-		</div>
-	);
+        <Route path="*" element={<NotFound />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,29 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import { Toaster } from "./components/ui/sonner";
-import ScrollToTop from "./ScrollToTop";
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { Toaster } from './components/ui/sonner';
+import './index.css';
+import ScrollToTop from './ScrollToTop';
+import store from './store/store';
 
-createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<ScrollToTop />
-		<Provider store={store}>
-			<App />
-			<Toaster />
-		</Provider>
-	</BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <ScrollToTop />
+    <Provider store={store}>
+      <Toaster
+        toastOptions={{
+          style: {
+            backgroundColor: '#1a1a1a',
+            color: '#fff',
+            borderRadius: '8px',
+          },
+          className: 'my-custom-toast',
+          duration: 3000,
+        }}
+      />
+
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
