21a10a0d0ffa2a3569f78210a8e51cc098f13bad
Shithi Moni <shithim555@gmail.com>
2025-11-20 14:39:17 +0600

    Update store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,38 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth-slice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth-slice';
 
-import AdminProductsSlice from "./admin/products-slice";
-import AdminAuctionProductsSlice from "./admin/auction-products-slice";
-import AdminOrderSlice from "./admin/order-slice";
+import AdminAuctionProductsSlice from './admin/auction-products-slice';
+import AdminFeedbackSlice from './admin/feedback-slice';
+import adminNotificationSlice from './admin/notification-slice';
+import AdminOrderSlice from './admin/order-slice';
+import AdminProductsSlice from './admin/products-slice';
+import AdminUserSlice from './admin/user-slice';
 
-import ShoppingProductsSlice from "./shop/products-slice";
-import ShoppingAuctionProductsSlice from "./shop/auction-products-slice";
-import ShoppingCartSlice from "./shop/cart-slice";
-import ShopAddressSlice from "./shop/address-slice";
-import shopOrderSlice from "./shop/order-slice";
-import shopSearchSlice from "./shop/search-slice";
+import ShopAddressSlice from './shop/address-slice';
+import ShoppingAuctionProductsSlice from './shop/auction-products-slice';
+import ShoppingCartSlice from './shop/cart-slice';
+import shopFeedbackSlice from './shop/feedback-slice';
+import shopOrderSlice from './shop/order-slice';
+import ShoppingProductsSlice from './shop/products-slice';
+import shopReviewSlice from './shop/review-slice';
+import shopSearchSlice from './shop/search-slice';
 
-import AuctionSlice from "./shop/auction-slice";
-import AuctionCheckoutSlice from "./shop/auction-checkout-slice";
+import AuctionCheckoutSlice from './shop/auction-checkout-slice';
+import AuctionSlice from './shop/auction-slice';
 
 const store = configureStore({
-	reducer: {
-		auth: authReducer,
+  reducer: {
+    auth: authReducer,
 
-		adminProducts: AdminProductsSlice,
-		adminAuctionProduct: AdminAuctionProductsSlice,
-		adminOrder: AdminOrderSlice,
+    adminProducts: AdminProductsSlice,
+    adminAuctionProduct: AdminAuctionProductsSlice,
+    adminOrder: AdminOrderSlice,
+    adminUser: AdminUserSlice,
+    adminNotifications: adminNotificationSlice,
+    adminFeedback: AdminFeedbackSlice,
 
-		shopProducts: ShoppingProductsSlice,
-		shopAuctionProducts: ShoppingAuctionProductsSlice,
-		shopCart: ShoppingCartSlice,
-		shopAddress: ShopAddressSlice,
-		shopOrder: shopOrderSlice,
-		shopSearch: shopSearchSlice,
+    shopProducts: ShoppingProductsSlice,
+    shopAuctionProducts: ShoppingAuctionProductsSlice,
+    shopCart: ShoppingCartSlice,
+    shopAddress: ShopAddressSlice,
+    shopOrder: shopOrderSlice,
+    shopSearch: shopSearchSlice,
+    shopReview: shopReviewSlice,
+    shopFeedback: shopFeedbackSlice,
 
-		shopAuction: AuctionSlice,
-		auctionCheckout: AuctionCheckoutSlice,
-	},
+    shopAuction: AuctionSlice,
+    auctionCheckout: AuctionCheckoutSlice,
+  },
 });
 
 export default store;
