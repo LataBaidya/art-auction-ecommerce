78b4cc517bb292a092a823be344ac8afb7d0cbae
Shithi Moni <shithim555@gmail.com>
2025-11-20 14:07:01 +0600

    order-slice update

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -1,115 +1,91 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-	orderList: [],
-	userList: [],
-	orderDetails: null,
+  orderList: [],
+  orderDetails: null,
 };
 
-export const getAllUsers = createAsyncThunk("order/getAllUsers", async () => {
-	const response = await axios.get(
-		"http://localhost:5000/api/admin/orders/users/get"
-	);
-	return response.data;
+export const getAllOrdersForAdmin = createAsyncThunk('order/getAllOrdersForAdmin', async () => {
+  const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/admin/orders/get`);
+  return response.data;
 });
 
-export const getAllOrdersForAdmin = createAsyncThunk(
-	"order/getAllOrdersForAdmin",
-	async () => {
-		const response = await axios.get(
-			"http://localhost:5000/api/admin/orders/get"
-		);
-		return response.data;
-	}
-);
-
 export const getAllAuctionOrdersForAdmin = createAsyncThunk(
-	"order/getAllAuctionOrdersForAdmin",
-	async () => {
-		const response = await axios.get(
-			"http://localhost:5000/api/admin/orders/auction-order/get"
-		);
-		return response.data;
-	}
+  'order/getAllAuctionOrdersForAdmin',
+  async () => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL}/admin/orders/auction-order/get`
+    );
+    return response.data;
+  }
 );
 
 export const fetchOrderDetailsForAdmin = createAsyncThunk(
-	"order/fetchOrderDetailsForAdmin",
-	async (id) => {
-		const response = await axios.get(
-			`http://localhost:5000/api/admin/orders/details/${id}`
-		);
-		return response.data;
-	}
+  'order/fetchOrderDetailsForAdmin',
+  async (id) => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL}/admin/orders/details/${id}`
+    );
+    return response.data;
+  }
 );
 
 export const updateOrderStatus = createAsyncThunk(
-	"order/updateOrderStatus",
-	async ({ id, orderStatus }) => {
-		const response = await axios.put(
-			`http://localhost:5000/api/admin/orders/update/${id}`,
-			{ orderStatus }
-		);
-		return response.data;
-	}
+  'order/updateOrderStatus',
+  async ({ id, orderStatus }) => {
+    const response = await axios.put(
+      `${import.meta.env.VITE_API_BASE_URL}/admin/orders/update/${id}`,
+      { orderStatus }
+    );
+    return response.data;
+  }
 );
 
 const adminOrderSlice = createSlice({
-	name: "adminOrderSlice",
-	initialState,
-	reducers: {
-		resetOrderDetails: (state) => {
-			state.orderDetails = null;
-		},
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(getAllUsers.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(getAllUsers.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.userList = action.payload.data;
-			})
-			.addCase(getAllUsers.rejected, (state) => {
-				state.isLoading = false;
-				state.userList = [];
-			})
-			.addCase(getAllOrdersForAdmin.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.orderList = action.payload.data;
-			})
-			.addCase(getAllOrdersForAdmin.rejected, (state) => {
-				state.isLoading = false;
-				state.orderList = [];
-			})
-			.addCase(getAllAuctionOrdersForAdmin.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(getAllAuctionOrdersForAdmin.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.orderList = action.payload.data;
-			})
-			.addCase(getAllAuctionOrdersForAdmin.rejected, (state) => {
-				state.isLoading = false;
-				state.orderList = [];
-			})
-			.addCase(fetchOrderDetailsForAdmin.pending, (state) => {
-				state.isLoading = true;
-			})
-			.addCase(fetchOrderDetailsForAdmin.fulfilled, (state, action) => {
-				state.isLoading = false;
-				state.orderDetails = action.payload.data;
-			})
-			.addCase(fetchOrderDetailsForAdmin.rejected, (state) => {
-				state.isLoading = false;
-				state.orderDetails = null;
-			});
-	},
+  name: 'adminOrderSlice',
+  initialState,
+  reducers: {
+    resetOrderDetails: (state) => {
+      state.orderDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(getAllAuctionOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllAuctionOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllAuctionOrdersForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(fetchOrderDetailsForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchOrderDetailsForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(fetchOrderDetailsForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderDetails = null;
+      });
+  },
 });
 
 export const { resetOrderDetails } = adminOrderSlice.actions;
